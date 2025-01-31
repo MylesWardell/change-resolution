fb4031b95120b27c54e960c2b389a56512cb07e2
Myles Wardell <myles.wardell@abx.com>
2025-01-31 10:25:00 +1000

    first commit

---
+++ main.ts
@@ -0,0 +1,79 @@
+type Resolution = [number, number];
+type ResolutionString = `${number}/${number}`;
+type ResolutionName = "Native" | "16:10" | "4:3";
+
+const NameToValue: Record<ResolutionName, Resolution> = {
+  "Native": [3840, 2160],
+  "16:10": [2560, 1600],
+  "4:3": [2048, 1536],
+};
+
+const ValueToName = new Map(
+  Object.entries(NameToValue).map(
+    ([key, value]) =>
+      [value.join("/"), key] as [ResolutionString, ResolutionName],
+  ),
+);
+
+const setResolution = async (resolution: ResolutionName) => {
+  const [width, height] = NameToValue[resolution];
+
+  const command = new Deno.Command("powershell", {
+    args: [`./ChangeScreenResolution.exe /w=${width} /h=${height} /f=240 /d=0`],
+  });
+  const { code, stdout } = await command.output();
+
+  // error
+  if (!code) {
+    const text = new TextDecoder().decode(stdout);
+    return console.error(text);
+  }
+
+  console.log(
+    "Successfully changed resolution to",
+    resolution,
+    [width, height].join("/"),
+  );
+};
+
+const decodeText = (text: string) => parseInt(text.split("\r")[0].trim());
+
+const getResolution = async (): Promise<ResolutionName | undefined> => {
+  const command = new Deno.Command("powershell", {
+    args: ["Get-DisplayResolution"],
+  });
+
+  const { stdout } = await command.output();
+  const resolutions = new TextDecoder().decode(stdout).split(":");
+  const resolution = [decodeText(resolutions[3]), decodeText(resolutions[4])]
+    .join("/") as ResolutionString;
+  const resolutionName = ValueToName.get(resolution);
+  console.log(
+    "Current Resolution",
+    resolutionName,
+    resolution,
+  );
+
+  return resolutionName;
+};
+
+const main = async () => {
+  const resolution = await getResolution();
+
+  if (!resolution) {
+    throw new Error("Failed to get valid resolution");
+  }
+
+  if (resolution === "Native") {
+    await setResolution("16:10");
+  }
+
+  if (resolution === "16:10") {
+    await setResolution("Native");
+  }
+};
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  await main();
+}
